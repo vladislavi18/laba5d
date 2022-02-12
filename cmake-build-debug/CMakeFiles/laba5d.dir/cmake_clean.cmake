bb8f1f8686c2624229a1981ecaa2766005462976
vladislavi_18 <ilcvlad922@gmail.com>
2022-02-12 22:44:44 +0300

    first commit

---
+++ cmake-build-debug/CMakeFiles/laba5d.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/laba5d.dir/main.c.obj"
+  "laba5d.exe"
+  "laba5d.exe.manifest"
+  "laba5d.pdb"
+  "liblaba5d.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/laba5d.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
