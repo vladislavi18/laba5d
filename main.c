43c61c8908986df62b41d830abbc395285a2d9d4
vladislavi_18 <ilcvlad922@gmail.com>
2022-02-14 23:48:24 +0300

    task7

--- main.c
+++ main.c
@@ -374,15 +374,48 @@ bool task6(matrix m1, matrix m2) {
     return (isEMatrix(mulMatrices(m1, m2)));
 }
 
+int max2(int a, int b) {
+    return a > b ? a : b;
+}
+
+int task7(matrix m) {
+    int sumArray[m.nCols + m.nRows - 2];
+    int k = 0;
+    for (size_t i = 1, j = 0; i < m.nRows; i++, j++) {
+        int max = m.values[i][0];
+        int duplicate = i;
+        while (i + 1 < m.nRows && j + 1 < m.nCols) {
+            j++;
+            i++;
+            max = max2(max, m.values[i][j]);
+        }
+        sumArray[k++] = max;
+        j = 0;
+        i = duplicate;
+    }
+
+    for (size_t i = 1, j = 0; i < m.nCols; i++, j++) {
+        int max = m.values[0][i];
+        int duplicate = i;
+        while (i + 1 < m.nCols && j + 1 < m.nRows) {
+            j++;
+            i++;
+            max = max2(max, m.values[j][i]);
+        }
+        sumArray[k++] = max;
+        j = 0;
+        i = duplicate;
+    }
+    int sum = sumInArray(sumArray, m.nRows + m.nCols - 2);
+    return sum;
+}
+
 int main() {
     test();
-    matrix m1 = createMatrixFromArray((int[]) {2, 5, 7,
-                                               6, 3, 4,
-                                               5, -2, -3,}, 3, 3);
-    matrix m2 = createMatrixFromArray((int[]) {1, -1, 1,
-                                               -38, 41, -34,
-                                               27, -29, 24,}, 3, 3);
-    printf("%d", task6(m1, m2));
+    matrix m = createMatrixFromArray((int[]) {3, 2, 5, 4,
+                                              5, 3, 6, 3,
+                                              3, 2, 1, 2}, 3, 4);
+    printf("%d", task7(m));
 
     return 0;
 }
