78834f15d62f14b7e07d94751b019ee8967e48f9
vladislavi_18 <ilcvlad922@gmail.com>
2022-02-20 23:16:52 +0300

    add tests for tasks

--- main.c
+++ main.c
@@ -351,7 +351,7 @@ void test_getMaxValuePos() {
     assert(maxPos.colIndex == expectedMaxPos.colIndex);
 }
 
-void test() {
+void testFunction() {
     test_isSymmetricMatrix();
     test_transposeSquareMatrix();
     test_getMinValuePos();
@@ -370,12 +370,45 @@ void task1(matrix m) {
     swapRows(m, maxIndex.rowIndex, minIndex.rowIndex);
 }
 
+void test_task1() {
+    matrix m = createMatrixFromArray((int[]) {1, 2, 3,
+                                              4, 5, 6,
+                                              7, 8, 9}, 3, 3);
+    task1(m);
+    matrix expectedMatrix = createMatrixFromArray((int[]) {7, 8, 9,
+                                                           4, 5, 6,
+                                                           1, 2, 3}, 3, 3);
+    assert(twoMatricesEqual(m, expectedMatrix));
+}
+
 void task2(matrix m) {
     insertionSortRowsMatrixByRowCriteria(m, maxInArray);
 }
 
+void test_task2() {
+    matrix m = createMatrixFromArray((int[]) {7, 1, 2,
+                                              1, 8, 1,
+                                              3, 2, 3}, 3, 3);
+    task2(m);
+    matrix expectedMatrix = createMatrixFromArray((int[]) {3, 2, 3,
+                                                           7, 1, 2,
+                                                           1, 8, 1}, 3, 3);
+    assert(twoMatricesEqual(m, expectedMatrix));
+}
+
 void task3(matrix m) {
     insertionSortColsMatrixByColCriteria(m, minInArray);
+}
+
+void test_task3() {
+    matrix m = createMatrixFromArray((int[]) {3, 5, 2, 4, 3, 3,
+                                              2, 5, 1, 8, 2, 7,
+                                              6, 1, 4, 4, 8, 3}, 3, 6);
+    task3(m);
+    matrix expectedMatrix = createMatrixFromArray((int[]) {5, 2, 3, 3, 3, 4,
+                                                           5, 1, 2, 2, 7, 8,
+                                                           1, 4, 6, 8, 3, 4}, 3, 6);
+    assert(twoMatricesEqual(m, expectedMatrix));
 }
 
 matrix task4(matrix m) {
@@ -383,6 +416,35 @@ matrix task4(matrix m) {
         return mulMatrices(m, m);
     else
         return m;
+}
+
+void test_task4_MatrixIsSymmetric() {
+    matrix m = createMatrixFromArray((int[]) {7, 1, 2,
+                                              1, 8, 17,
+                                              2, 17, 3}, 3, 3);
+    m = task4(m);
+    matrix expectedMatrix = createMatrixFromArray((int[]) {54, 49, 37,
+                                                           49, 354, 189,
+                                                           37, 189, 302}, 3, 3);
+
+    assert(twoMatricesEqual(m, expectedMatrix));
+}
+
+void test_task4_MatrixIsNotSymmetric() {
+    matrix m = createMatrixFromArray((int[]) {7, 3, 2,
+                                              1, 8, 17,
+                                              8, 17, 3}, 3, 3);
+    m = task4(m);
+    matrix expectedMatrix = createMatrixFromArray((int[]) {7, 3, 2,
+                                                           1, 8, 17,
+                                                           8, 17, 3}, 3, 3);
+
+    assert(twoMatricesEqual(m, expectedMatrix));
+}
+
+void test_task4() {
+    test_task4_MatrixIsSymmetric();
+    test_task4_MatrixIsNotSymmetric();
 }
 
 int sumInArray(int *a, size_t size) {
@@ -411,11 +473,69 @@ matrix task5(matrix m) {
     return m;
 }
 
+void test_task5_SumsNotEqual() {
+    matrix m = createMatrixFromArray((int[]) {2, 13, 4,
+                                              1, 8, 17,
+                                              23, 47, 3}, 3, 3);
+    task5(m);
+    matrix expectedMatrix = createMatrixFromArray((int[]) {2, 1, 23,
+                                                           13, 8, 47,
+                                                           4, 17, 3}, 3, 3);
+
+    assert(twoMatricesEqual(m, expectedMatrix));
+}
+
+void test_task5_SumsEqual() {
+    matrix m = createMatrixFromArray((int[]) {10, 13, 3,
+                                              1, 8, 17,
+                                              23, 47, 3}, 3, 3);
+    task5(m);
+    matrix expectedMatrix = createMatrixFromArray((int[]) {10, 13, 3,
+                                                           1, 8, 17,
+                                                           23, 47, 3}, 3, 3);
+
+    assert(twoMatricesEqual(m, expectedMatrix));
+}
+
+void test_task5() {
+    test_task5_SumsNotEqual();
+    test_task5_SumsEqual();
+}
+
 bool task6(matrix m1, matrix m2) {
     matrix mulMatrix = mulMatrices(m1, m2);
     bool EMatrix = isEMatrix(mulMatrix);
     freeMemMatrix(mulMatrix);
     return EMatrix;
+}
+
+void test_task6_IsEMatrix() {
+    matrix m1 = createMatrixFromArray((int[]) {2, 5, 7,
+                                               6, 3, 4,
+                                               5, -2, -3}, 3, 3);
+
+    matrix m2 = createMatrixFromArray((int[]) {1, -1, 1,
+                                               -38, 41, -34,
+                                               27, -29, 24}, 3, 3);
+
+    assert(task6(m1, m2) == true);
+}
+
+void test_task6_IsNotEMatrix() {
+    matrix m1 = createMatrixFromArray((int[]) {2, 5, 7,
+                                               6, 3, 4,
+                                               5, -2, -3}, 3, 3);
+
+    matrix m2 = createMatrixFromArray((int[]) {1, 4, 1,
+                                               -38, 35, -34,
+                                               27, -27, 24}, 3, 3);
+
+    assert(task6(m1, m2) == false);
+}
+
+void test_task6() {
+    test_task6_IsEMatrix();
+    test_task6_IsNotEMatrix();
 }
 
 int max2(int a, int b) {
@@ -437,6 +557,13 @@ int task7(matrix m) {
     }
     int sum = sumInArray(sumArray, m.nRows + m.nCols - 1);
     return sum;
+}
+
+void test_task7() {
+    matrix m = createMatrixFromArray((int[]) {3, 2, 5, 4,
+                                              1, 3, 6, 3,
+                                              3, 2, 1, 2}, 3, 4);
+    assert(task7(m) == 20);
 }
 
 int min2(int a, int b) {
@@ -462,6 +589,27 @@ int task8(matrix m) {
     return min;
 }
 
+void test_task8_NormArea() {
+    matrix m = createMatrixFromArray((int[]) {6, 8, 9, 2,
+                                              7, 12, 3, 4,
+                                              10, 11, 5, 1}, 3, 4);
+
+    assert(task8(m) == 6);
+}
+
+void test_task8_singleElementArea() {
+    matrix m = createMatrixFromArray((int[]) {17, 7, 5, 6,
+                                              3, 11, 8, 9,
+                                              4, 1, 12, 2}, 3, 4);
+
+    assert(task8(m) == 17);
+}
+
+void test_task8() {
+    test_task8_NormArea();
+    test_task8_singleElementArea();
+}
+
 float getDistance(int *a, int size) {
     int distance = 0;
     for (size_t i = 0; i < size; i++) {
@@ -472,6 +620,18 @@ float getDistance(int *a, int size) {
 
 void sortByDistance(matrix m) {
     insertionSortRowsMatrixByRowCriteriaF(m, getDistance);
+}
+
+void test_task9() {
+    matrix m = createMatrixFromArray((int[]) {17, 7, 5, 6,
+                                              3, 11, 8, 9,
+                                              4, 1, 12, 2}, 3, 4);
+    sortByDistance(m);
+    matrix expectedMatrix = createMatrixFromArray((int[]) {4, 1, 12, 2,
+                                                           3, 11, 8, 9,
+                                                           17, 7, 5, 6}, 3, 4);
+    assert(twoMatricesEqual(m, expectedMatrix));
+
 }
 
 int cmp_long_long(const void *pa, const void *pb) {
@@ -488,7 +648,10 @@ int cmp_long_long(const void *pa, const void *pb) {
 
 int countNUnique(long long *a, int n) {
     qsort(a, n, sizeof(long long), cmp_long_long);
-    int count = 1;
+    int count;
+    if (n != 0) {
+        count = 1;
+    }
     int i = 1;
     while (i < n) {
         if (a[i - 1] != a[i])
@@ -504,6 +667,35 @@ int countEqClassesByRowsSum(matrix m) {
         rowsSum[i] = sumInArray(m.values[i], m.nCols);
     }
     return countNUnique(rowsSum, m.nRows);
+}
+
+void test_task10_() {
+    matrix m = createMatrixFromArray((int[]) {7, 1,
+                                              2, 7,
+                                              5, 4,
+                                              4, 3,
+                                              1, 6,
+                                              8, 0}, 6, 2);
+
+    assert(countEqClassesByRowsSum(m) == 3);
+
+}
+
+void test_task10_allSumsIsEqual() {
+    matrix m = createMatrixFromArray((int[]) {7, 1,
+                                              3, 5,
+                                              4, 4,
+                                              2, 6,
+                                              1, 7,
+                                              8, 0}, 6, 2);
+
+    assert(countEqClassesByRowsSum(m) == 1);
+
+}
+
+void test_task10() {
+    test_task10_();
+    test_task10_allSumsIsEqual();
 }
 
 int getNSpecialElement(matrix m) {
@@ -522,6 +714,29 @@ int getNSpecialElement(matrix m) {
             nSpecial++;
     }
     return nSpecial;
+}
+
+void test_task11_thereElements() {
+    matrix m = createMatrixFromArray((int[]) {3, 5, 5, 4,
+                                              2, 3, 6, 7,
+                                              12, 2, 1, 2}, 3, 4);
+
+    assert(getNSpecialElement(m) == 2);
+
+}
+
+void test_task11_zeroSpecialElements() {
+    matrix m = createMatrixFromArray((int[]) {3, 5, 5, 5,
+                                              2, 3, 6, 7,
+                                              4, 2, 1, 2}, 3, 4);
+
+    assert(getNSpecialElement(m) == 0);
+
+}
+
+void test_task11() {
+    test_task11_thereElements();
+    test_task11_zeroSpecialElements();
 }
 
 position getLeftMin(matrix m) {
@@ -549,6 +764,18 @@ void swapPenultimateRow(matrix m) {
     memcpy(m.values[m.nRows - 2], col, sizeof(int) * m.nCols);
 }
 
+void test_task12() {
+    matrix m = createMatrixFromArray((int[]) {1, 2, 3,
+                                              4, 5, 6,
+                                              7, 8, 1}, 3, 3);
+    swapPenultimateRow(m);
+    matrix expectedMatrix = createMatrixFromArray((int[]) {1, 2, 3,
+                                                           1, 4, 7,
+                                                           7, 8, 1}, 3, 3);
+    assert(twoMatricesEqual(m, expectedMatrix));
+
+}
+
 bool isNonDescendingSorted(int *a, size_t size) {
     for (size_t i = 1; i < size; i++) {
         if (a[i] < a[i - 1])
@@ -571,6 +798,26 @@ int countNonDescendingRowsMatrices(matrix *ms, int nMatrix) {
         numberRightMatrix += hasAllNonDescendingRows(ms[i]);
     }
     return numberRightMatrix;
+}
+
+void test_task13() {
+    matrix *ms = createArrayOfMatrixFromArray(
+            (int[]) {
+                    7, 1,
+                    1, 1,
+
+                    1, 6,
+                    2, 2,
+
+                    5, 4,
+                    2, 3,
+
+                    1, 3,
+                    7, 9
+            },
+            4, 2, 2);
+
+    assert(countNonDescendingRowsMatrices(ms, 4) == 2);
 }
 
 int countValues(const int *a, size_t size, int value) {
@@ -602,6 +849,18 @@ void printMatrixWithMaxZeroRows(matrix *ms, int nMatrix) {
             outputMatrix(ms[i]);
 }
 
+void test_countZeroRows() {
+    matrix m = createMatrixFromArray(
+            (int[]) {
+                    0, 0,
+                    1, 0,
+                    0, 0,
+            },
+            3, 2);
+
+    assert(countZeroRows(m) == 2);
+}
+
 int maxInMatrix(matrix m) {
     int norm = abs(m.values[0][0]);
     for (size_t i = 0; i < m.nRows; i++) {
@@ -624,12 +883,25 @@ void printMatricesWithMinNorm(matrix *ms, int nMatrix) {
     }
 }
 
-int main() {
-    test();
-    matrix m = createMatrixFromArray((int[]) {3, 2, 5, 4,
-                                              1, 3, 6, 3,
-                                              3, 2, 1, 2}, 3, 4);
+void test_tasks() {
+    test_task1();
+    test_task2();
+    test_task3();
+    test_task4();
+    test_task5();
+    test_task6();
+    test_task7();
+    test_task8();
+    test_task9();
+    test_task10();
+    test_task11();
+    test_task12();
+    test_task13();
+    test_countZeroRows();
+}
 
-    printf("%d", task7(m));
+int main() {
+    testFunction();
+    test_tasks();
     return 0;
 }
